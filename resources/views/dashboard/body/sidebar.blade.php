108a32780bb5096fbccf5b9fcc57883ce6bf5252
zeshan-ashraf <malikzeshanashraf@gmail.com>
2026-03-03 17:13:34 +0500

    p and loss UI and print view fix

--- resources/views/dashboard/body/sidebar.blade.php
+++ resources/views/dashboard/body/sidebar.blade.php
@@ -11,15 +11,23 @@
     <div class="data-scrollbar" data-scroll="1">
         <nav class="iq-sidebar-menu">
             <ul id="iq-sidebar-toggle" class="iq-menu">
-                <li class="{{ Request::is('dashboard') ? 'active' : '' }}">
+                <!--<li class="{{ Request::is('dashboard') ? 'active' : '' }}">
                     <a href="{{ route('dashboard') }}" class="svg-icon">
                         <svg  class="svg-icon" id="p-dash1" width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                             <path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"></path><polyline points="3.27 6.96 12 12.01 20.73 6.96"></polyline><line x1="12" y1="22.08" x2="12" y2="12"></line>
                         </svg>
                         <span class="ml-4">Dashboards</span>
                     </a>
+                </li>-->
+                <li class="{{ Request::is('dashboard') ? 'active' : '' }}">
+                    <a href="{{ route('super-admin.dashboard') }}" class="svg-icon">
+                        <svg  class="svg-icon" id="p-dash1" width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"></path><polyline points="3.27 6.96 12 12.01 20.73 6.96"></polyline><line x1="12" y1="22.08" x2="12" y2="12"></line>
+                        </svg>
+                        <span class="ml-4">Dashboards</span>
+                    </a>
                 </li>
-
+                
                 @if (auth()->user()->can('advance.pos.menu'))
                 <li class="{{ Request::is('invoice*') ? 'active' : '' }}">
                     <a href="{{ route('invoice.create') }}" class="svg-icon">
